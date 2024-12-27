742b55ca40464bddb621f787a4bbc70780d26035
Rory Waite <aurelien.waite@gmail.com>
2024-12-27 21:16:06 +0100

    Removing spark code

--- run_alpha.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-RUN_DIR=$(dirname $0)
-
-docker run -it --rm \
-    -v $RUN_DIR:/aggregator \
-    -p 8815:8815 \
-    aurelienwaite/aggregator-toy:alpha \
-    python /aggregator/alpha_example.py
